f81ae9dc5b5c35c071a83073f368bcb7daa416cb
onevic <onv_vic@vi>
2013-11-21 18:13:54 +0800

    1

---
+++ SportsShopping/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  SportsShopping
+//
+//  Created by scott on 13-11-21.
+//  Copyright (c) 2013年 scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
